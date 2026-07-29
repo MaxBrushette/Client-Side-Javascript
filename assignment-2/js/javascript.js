981c389eaf0d86f578e87f75a409d8f4ee948b63
MaxBrushette <emilycat167@gmail.com>
2026-07-28 20:45:19 -0400

    action is for server-side. Removed it from the form

--- assignment-2/js/javascript.js
+++ assignment-2/js/javascript.js
@@ -1,8 +1,10 @@
 const output = document.querySelector("#output");
+const form = document.querySelector("#pizza-creation");
 
 class Pizza {
-    constructor(name,size,sauce,toppings,specialty){
-        this.name = name;
+    constructor(firstName,lastName,size,sauce,toppings,specialty){
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.size = size;
         this.sauce = sauce;
         this.toppings = toppings;
