1d0cacdb5336dc3b6e1ebf4a82862f119294ebb4
MaxBrushette <emilycat167@gmail.com>
2026-07-28 21:15:11 -0400

    creating the order() and event listener.

--- assignment-2/js/javascript.js
+++ assignment-2/js/javascript.js
@@ -10,4 +10,37 @@ class Pizza {
         this.toppings = toppings;
         this.specialty = specialty
     }
+    order(){
+        return `
+        Name: ${this.firstName + " " + this.lastName}
+        Size: ${this.size}
+        Sauce: ${this.sauce}
+        Toppings: ${this.toppings.length === 0 ? "None" : this.toppings.join(", ")}
+        Specialty: ${this.specialty}
+        `;
+    }
 }
+
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+
+    const firstName = document.getElementById("fname").value;
+    const lastName = document.getElementById("lname").value;
+    const size = document.querySelector('input[name="size"]:checked').value;
+    const sauce = document.querySelector('input[name="sauce"]:checked').value;
+    const toppings = Array.from(
+        document.querySelectorAll('input[name="topping[]"]:checked'),
+        topping=>topping.value
+    );
+    const specialty = document.querySelector('input[name="specialty"]:checked').value;
+
+    const pizza = new Pizza(
+        firstName,
+        lastName,
+        size,
+        sauce,
+        toppings,
+        specialty
+    );
+    output.textContent = pizza.order();
+});
