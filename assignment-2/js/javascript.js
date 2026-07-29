62c565c81e8de687d71983c520bc815d194ea9f3
MaxBrushette <emilycat167@gmail.com>
2026-07-28 21:28:17 -0400

    Comments + CSS

--- assignment-2/js/javascript.js
+++ assignment-2/js/javascript.js
@@ -1,6 +1,6 @@
 const output = document.querySelector("#output");
 const form = document.querySelector("#pizza-creation");
-
+//Creating the class constructor and the order() function.
 class Pizza {
     constructor(firstName,lastName,size,sauce,toppings,specialty){
         this.firstName = firstName;
@@ -10,6 +10,8 @@ class Pizza {
         this.toppings = toppings;
         this.specialty = specialty
     }
+    //Order function that displays the order at the bottom of the page.
+    //Ternary operator is used to see that if the length of the toppings array is 0, it'll display "None" instead of just a blank "Toppings: "
     order(){
         return `
         Name: ${this.firstName + " " + this.lastName}
@@ -20,10 +22,11 @@ class Pizza {
         `;
     }
 }
-
+//Event listener. The prevent default makes it so it doesn't actually submit. Array.from makes an array from each of the selected toppings.
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
 form.addEventListener("submit",function(event){
     event.preventDefault();
-
+    //These fetch the values of the form (checked for radios/checkboxes)
     const firstName = document.getElementById("fname").value;
     const lastName = document.getElementById("lname").value;
     const size = document.querySelector('input[name="size"]:checked').value;
@@ -33,7 +36,7 @@ form.addEventListener("submit",function(event){
         topping=>topping.value
     );
     const specialty = document.querySelector('input[name="specialty"]:checked').value;
-
+//creates a pizza obj using the constructor.
     const pizza = new Pizza(
         firstName,
         lastName,
