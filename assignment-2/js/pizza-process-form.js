ca83e89ad6ce46c2a9267c967f9521e4eb81cade
MaxBrushette <emilycat167@gmail.com>
2026-07-28 20:25:24 -0400

    html form

---
+++ assignment-2/js/pizza-process-form.js
@@ -0,0 +1,9 @@
+const output = document.querySelector("#output");
+
+function Pizza(name,size,sauce,toppings,specialty){
+    this.name = name;
+    this.size = size;
+    this.sauce = sauce;
+    this.toppings = toppings;
+    this.specialty = specialty;
+}
