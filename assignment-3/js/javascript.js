226b6f23d4e642dd980e52777229a11de887b69c
MaxBrushette <emilycat167@gmail.com>
2026-08-11 23:57:11 -0400

    style + javascript update for types.

--- assignment-3/js/javascript.js
+++ assignment-3/js/javascript.js
@@ -29,9 +29,59 @@ function getPokemon(event){
 function displayPokemon(json){
     console.log(json);
     let pokeName = json.name;
+    //V this is to capitalize the pokemon's name.
     pokeName = pokeName.charAt(0).toUpperCase() + pokeName.slice(1);
-    card.querySelector("p").textContent=json.id;
+    card.querySelector("#dex").textContent=json.id;
     card.querySelector("h2").textContent=pokeName;
     pokeImg.src=json.sprites.front_default;
     pokeImg.alt=json.name;
+    //this makes it so I can get both types if a pokemon has more than one.
+    let types = json.types.map(type=>{
+        return type.type.name;
+    });
+    //this gets only the first type.
+    let type1=json.types[0].type.name;
+    card.querySelector("#type").textContent=`${types.join("/")}`;
+    if( type1 == "grass"){
+        card.style.background=`green`;
+    }
+    else if( type1 == "fire"){
+        card.style.background=`red`;
+    }
+    else if(type1=="water"){
+        card.style.background=`blue`;
+    }
+    else if(type1=="electric"){
+        card.style.background=`yellow`;
+    }
+    else if(type1=="psychic"){
+        card.style.background=`purple`;
+    }
+    else if(type1=="ground"){
+        card.style.background=`brown`;
+    }
+    else if(type1=="fighting"){
+        card.style.background=`orange`;
+    }
+    else if(type1=="rock"){
+        card.style.background=`gray`;
+    }
+    else if(type1=="dark"){
+        card.style.background=`darkslategray`;
+    }
+    else if(type1=="dragon"){
+        card.style.background=`darkblue`;
+    }
+    else if(type1=="fairy"){
+        card.style.background=`pink`;
+    }
+    else if(type1=="flying"){
+        card.style.background=`lightcyan`;
+    }
+    else if(type1="ghost"){
+        card.style.background=`mediumpurple`;
+    }
+    else{
+        card.style.background=`rebeccapurple`;
+    }
 }
